1273a4248d99f4d0c3dd22e5a5897c06edf80cf1
jeffcshelton <jeffrey.shelton.c@gmail.com>
2024-02-05 17:42:52 -0500

    Add IMU options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use spidev::{SpiModeFlags, Spidev, SpidevOptions};
+
 fn main() {
-    println!("Hello, space!");
+	let mut spi = Spidev::open("/dev/spidev0.0").unwrap();
+	let imu_options = SpidevOptions::new()
+		.lsb_first(false)
+		.bits_per_word(16)
+		.max_speed_hz(2_000_000)
+		.mode(SpiModeFlags::SPI_MODE_3)
+		.build();
+
+	spi.configure(&imu_options)
+		.expect("failed to configure SPI for the IMU");
+
+	println!("Hello, space!");
 }
