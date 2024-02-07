0a792247188d1465240e05530cb2149b695924b6
Tymur Tkachenko <ttkachenko3@gatech.edu>
2024-02-07 02:16:44 -0500

    SPI for 1 register

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,32 @@ fn main() {
 	spi.configure(&imu_options)
 		.expect("failed to configure SPI for the IMU");
 
-	println!("Hello, space!");
+	// read from X_GYRO_LOW (0x04)
+	// 0x8000 | (reg_addr << 8); reg_addr = 0x04
+	let tx_buf1 = [0x8400];
+	let mut rx_buf1 = [0; 1];
+
+	// read from X_GYRO_OUT (0x06)
+	let tx_buf2 = [0x8600];
+	let mut rx_buf2 = [0; 1];
+
+	// SPI
+	let result = {
+		let mut transfers = vec![
+			SpidevTransfer::write(&tx_buf1),
+			SpidevTransfer::read(&mut rx_buf1),
+			SpidevTransfer::write(&tx_buf2),
+			SpidevTransfer::read(&mut rx_buf2),
+		];
+		spidev.transfer_multiple(&mut transfers)
+	};
+
+	// final value = (high<<16) | low
+	match result {
+		Ok(_) => {
+			let result = rx_buf1 | (rx_buf2 << 16);
+			println!("{:?} received", result);
+		}
+		Err(err) => println!("{:?}", err),
+	}
 }
