9af3896f65ac05cb66fa993c24403de131a47b96
Tymur Tkachenko <ttkachenko3@gatech.edu>
2024-02-28 18:22:09 -0500

    removed file writing

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use spidev::{Spidev, SpidevOptions, SpiModeFlags};
 use spidev::spidevioctl::SpidevTransfer;
 use std::thread;
 use std::time::Duration;
-use std::fs::File;
-use std::io::Write;
+// use std::fs::File;
+// use std::io::Write;
 use gpio::{Gpio, Pin};
 use gpio::PinMode::Output;
 use gpio::PinValue::Low;
@@ -27,38 +27,38 @@ fn main() {
 	clk.mode(Output);
 	clk.digital_write(Low);
 
-	let mut f = match File::create("log.txt") {
-		Ok(f) => f,
-		Err(e) => {
-			panic!("{}", e);
-		}
-	};
+	// let mut f = match File::create("log.txt") {
+	// 	Ok(f) => f,
+	// 	Err(e) => {
+	// 		panic!("{}", e);
+	// 	}
+	// };
 
 	loop {
-		readspi2([0x1C, 0x00], &spi, String::from("TEMP"), &mut f);
-		readspi2([0x1E, 0x00], &spi, String::from("TIME"), &mut f);
+		readspi2([0x1C, 0x00], &spi, String::from("TEMP"));
+		readspi2([0x1E, 0x00], &spi, String::from("TIME"));
 
-		readspi([0x04, 0x00, 0x06, 0x00], &spi, String::from("X_GYRO"), &mut f);
-		readspi([0x08, 0x00, 0x0A, 0x00], &spi, String::from("Y_GYRO"), &mut f);
-		readspi([0x0C, 0x00, 0x0E, 0x00], &spi, String::from("Z_GYRO"), &mut f);
+		readspi([0x04, 0x00, 0x06, 0x00], &spi, String::from("X_GYRO"));
+		readspi([0x08, 0x00, 0x0A, 0x00], &spi, String::from("Y_GYRO"));
+		readspi([0x0C, 0x00, 0x0E, 0x00], &spi, String::from("Z_GYRO"));
 
-		readspi([0x10, 0x00, 0x12, 0x00], &spi, String::from("X_ACCL"), &mut f);
-		readspi([0x14, 0x00, 0x16, 0x00], &spi, String::from("Y_ACCL"), &mut f);
-		readspi([0x18, 0x00, 0x1A, 0x00], &spi, String::from("Z_ACCL"), &mut f);
+		readspi([0x10, 0x00, 0x12, 0x00], &spi, String::from("X_ACCL"));
+		readspi([0x14, 0x00, 0x16, 0x00], &spi, String::from("Y_ACCL"));
+		readspi([0x18, 0x00, 0x1A, 0x00], &spi, String::from("Z_ACCL"));
 
-		readspi([0x24, 0x00, 0x26, 0x00], &spi, String::from("X_DELTANG"), &mut f);
-		readspi([0x28, 0x00, 0x2A, 0x00], &spi, String::from("Y_DELTANG"), &mut f);
-		readspi([0x2C, 0x00, 0x2E, 0x00], &spi, String::from("Z_DELTANG"), &mut f);
+		readspi([0x24, 0x00, 0x26, 0x00], &spi, String::from("X_DELTANG"));
+		readspi([0x28, 0x00, 0x2A, 0x00], &spi, String::from("Y_DELTANG"));
+		readspi([0x2C, 0x00, 0x2E, 0x00], &spi, String::from("Z_DELTANG"));
 
-		readspi([0x30, 0x00, 0x32, 0x00], &spi, String::from("X_DELTVEL"), &mut f);
-		readspi([0x34, 0x00, 0x36, 0x00], &spi, String::from("Y_DELTVEL"), &mut f);
-		readspi([0x38, 0x00, 0x3A, 0x00], &spi, String::from("Z_DELTVEL"), &mut f);
+		readspi([0x30, 0x00, 0x32, 0x00], &spi, String::from("X_DELTVEL"));
+		readspi([0x34, 0x00, 0x36, 0x00], &spi, String::from("Y_DELTVEL"));
+		readspi([0x38, 0x00, 0x3A, 0x00], &spi, String::from("Z_DELTVEL"));
 
 		thread::sleep(Duration::from_secs(2));
 	}
 }
 
-fn readspi(tx_buf: [u8; 4], spi: &Spidev, s: String, f: &mut File) {
+fn readspi(tx_buf: [u8; 4], spi: &Spidev, s: String) {
 	let mut rx_buf = [0; 4];
 	let mut transfer = SpidevTransfer::read_write(&tx_buf, &mut rx_buf);
     let result = spi.transfer(&mut transfer);
@@ -69,10 +69,10 @@ fn readspi(tx_buf: [u8; 4], spi: &Spidev, s: String, f: &mut File) {
         Err(err) => println!("{:?}", err),
     }
 
-	f.write_all(&rx_buf).expect("Failed writing to file");
+	// f.write_all(&rx_buf).expect("Failed writing to file");
 }
 
-fn readspi2(tx_buf: [u8; 2], spi: &Spidev, s: String, f: &mut File) {
+fn readspi2(tx_buf: [u8; 2], spi: &Spidev, s: String) {
 	let mut rx_buf = [0; 2];
 	let mut transfer = SpidevTransfer::read_write(&tx_buf, &mut rx_buf);
     let result = spi.transfer(&mut transfer);
@@ -83,5 +83,5 @@ fn readspi2(tx_buf: [u8; 2], spi: &Spidev, s: String, f: &mut File) {
         Err(err) => println!("{:?}", err),
     }
 
-	f.write_all(&rx_buf).expect("Failed writing to file");
+	// f.write_all(&rx_buf).expect("Failed writing to file");
 }
